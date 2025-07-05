902c635f06d426ec9f0ff38ef066400e5fb51124
Samuel Ngombi <163398655+SamuelNgombi2704@users.noreply.github.com>
2025-07-05 12:19:58 +0200

    another commit message

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -28,7 +28,7 @@ export const Header = () => {
       <div className="container mx-auto px-6 py-4">
         <div className="flex items-center justify-between">
           <div className="flex items-center space-x-2 text-2xl font-bold bg-gradient-to-r from-yellow-400 to-orange-400 bg-clip-text text-yellow-400 font-black">
-            <img src="public\favicon.png" alt="Icon" className="w-11 h-11" />
+            <img src="assets\favicon.png" alt="Icon" className="w-11 h-11" />
             <span>PORTFOLIO</span>
           </div>
 
